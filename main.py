95de3816289b815b2229f973e57b572854219177
Dmytro Sirko <dimasirko@gmail.com>
2024-02-13 18:31:47 +0200

    added report

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+coffee_machine_is_on = True
+
+while coffee_machine_is_on:
+    user_answer = input("What would you like? (espresso/latte/cappuccino/): ").lower()
+    if user_answer == "report":
+        ingridients_report = CoffeeMaker()
+        ingridients_report.report()
+        money_report = MoneyMachine()
+        money_report.report()
