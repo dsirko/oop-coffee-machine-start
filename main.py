5f2d6818c5e5085bf033850be7e355ab7f8e0abb
Dmytro Sirko <dimasirko@gmail.com>
2024-02-14 23:18:29 +0200

    added all functions

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
+menu_items = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+drinks = menu_items.get_items()
 coffee_machine_is_on = True
 
 while coffee_machine_is_on:
-    user_answer = input("What would you like? (espresso/latte/cappuccino/): ").lower()
+    user_answer = input(f"What would you like? {drinks}: ").lower()
     if user_answer == "report":
-        ingridients_report = CoffeeMaker()
-        ingridients_report.report()
-        money_report = MoneyMachine()
-        money_report.report()
-    if user_answer == "off":
+        coffee_maker.report()
+        money_machine.report()
+    elif user_answer == "off":
         print("Coffee machine is going shot down...")
         coffee_machine_is_on = False
+    else:
+        drink = menu_items.find_drink(user_answer)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
